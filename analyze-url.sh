44610f639ff77f6d79f87dbb89e6c14c8da5bfb4
Eric Phetteplace <phette23@gmail.com>
2012-11-19 17:20:08 -0500

    better csv handling, less semicolons

--- analyze-url.sh
+++ analyze-url.sh
@@ -8,8 +8,8 @@ function usage() {
 }
 
 # no parameters
-if [ -z "$1" ]; then
-    usage
+if [ -z "$1" ]
+    then usage
     exit
 fi
 
@@ -22,10 +22,11 @@ while getopts :t:u:h opt; do
 	    TITLE="$OPTARG";;
 	u)
 	    # check for http(s) at the beginning, put there if not present
-	    if [[ "$OPTARG" =~ ^http://.* || "$OPTARG" =~ ^https://.* ]]; then
-		URL="$OPTARG"
+	    if [[ "$OPTARG" =~ ^http://.* || "$OPTARG" =~ ^https://.* ]]
+		then URL="$OPTARG"
 	    else
 		URL=http://"$OPTARG"
+		echo "Prepended http:// to URL"
 	    fi;;
 	h)
 	    usage
@@ -42,13 +43,15 @@ done
 # shorthand use-case: no -u param but $1
 if [ -z "$URL" ]; then
     # check for http(s) at the beginning, put there if not present
-    if [[ "$1" =~ ^http://.* || "$1" =~ ^https://.* ]]; then
-	URL="$1"
+    if [[ "$1" =~ ^http://.* || "$1" =~ ^https://.* ]]
+	then URL="$1"
     else
 	URL=http://"$1"
+	echo "Prepended http:// to URL: $URL"
     fi
 fi
 
+echo "Running YSlow on $TITLE, hang on a minute..."
 # see github.com/marcelduran/yslow/wiki/PhantomJS
 # these options are: info=grade, format=json
 phantomjs ./build/phantomjs/yslow.js -i grade -f json $URL > analysis.json
@@ -57,6 +60,7 @@ phantomjs ./build/phantomjs/yslow.js -i grade -f json $URL > analysis.json
 # Parse the YSlow JSON #
 ########################
 
+echo "Parsing results json."
 # overall grade
 OVERALL=$(jshon -e o < analysis.json)
 # no. of requests
@@ -74,8 +78,16 @@ MINIFY=$(jshon -e g -e yminify -e score < analysis.json)
 # Build analysis CSV #
 ######################
 
-touch analysis.csv
-# header row
-echo "Title,URL,Overall Grade,No. Requests,Requests Score,GZIP Grade,JS Bottom Grade,Minification Grade" >> analysis.csv
-# fill in values using input & YSlow results
+# create file with header row if it doesn't exist
+if [ ! -f analysis.csv ]
+    then echo "No analysis.csv! Creating..."
+    touch analysis.csv
+    echo "Title,URL,Overall Grade,No. Requests,Requests Score,GZIP Grade,JS Bottom Grade,Minification Grade" >> analysis.csv
+else
+    echo "Analysis.csv exists."
+fi
+
+echo "Appending YSlow results..."
+# fill in values using params & YSlow results
 echo "$TITLE,$URL,$OVERALL,$REQS,$REQSGRADE,$GZIP,$JSBOTTOM,$MINIFY" >> analysis.csv
+echo "Finished!"
