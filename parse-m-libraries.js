4254f73afdef8ae35ffde6a2e78a20a18738a1e4
Eric Phetteplace <phette23@gmail.com>
2012-11-19 17:19:20 -0500

    reformat to match json structure, stringify at end

--- parse-m-libraries.js
+++ parse-m-libraries.js
@@ -1,6 +1,6 @@
 // www.libsuccess.org/index.php?title=M-Libraries
 // object for storing site URLs & their link text
-var sites = {};
+var jsonoutput = { "sites": [] };
 
 // grab via ID
 $( '#Mobile_interfaces_\\.28and\\.2For_OPACS\\.29' )
@@ -13,6 +13,8 @@ $( '#Mobile_interfaces_\\.28and\\.2For_OPACS\\.29' )
     // get the links
     .find( 'li a' )
     // throw the hrefs & their link text in an object
-    .each( function () {
-        sites[ this.innerText ] = this.href;
+    .each( function ( i, el ) {
+	jsonoutput.sites.push( { title : this.innerText, url : this.href } );
     } );
+
+JSON.stringify( jsonoutput );
